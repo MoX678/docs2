19eb1f03f75197e92cf69985a6c685b62c06f8e5
MoX678 <mk6547719@gmail.com>
2025-11-27 11:17:31 +0200

    test ci

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "MetaCall Docs",
+  title: "Test Preview CI",
   tagline: "Documentation for MetaCall",
   favicon: "img/metacall-logo.png",
 
